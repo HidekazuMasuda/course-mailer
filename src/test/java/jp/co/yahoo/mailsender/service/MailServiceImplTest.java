8e7dab55758986a208accb67f9f310286d816937
MakotoIkeda <message.to.makoto@gmail.com>
2017-08-16 10:20:57 +0900

    add mailservice interface

--- src/test/java/jp/co/yahoo/mailsender/service/MailServiceImplTest.java
+++ src/test/java/jp/co/yahoo/mailsender/service/MailServiceImplTest.java
@@ -1,62 +1,60 @@
 package jp.co.yahoo.mailsender.service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailServiceImplTest {
 
-public class MailServiceTest {
+    @Autowired
+    private MailService service;
 
     @Test
     public void mailSendSuccess() throws Exception {
-        MailService service = new MailService();
         service.send("gadget.mailsender@gmail.com", "gadget.mailsender@gmail.com", "subject", "some body.");
     }
 
     @Test(expected = Exception.class)
     public void sendWithFromFieldEmpty() throws Exception {
-        MailService service = new MailService();
         service.send("", "gadget.mailsender@gmail.com", "subject", "some body.");
     }
 
     @Test(expected = Exception.class)
     public void sendWithToFieldEmpty() throws Exception {
-        MailService service = new MailService();
         service.send("gadget.mailsender@gmail.com", "", "subject", "some body.");
     }
 
     @Test(expected = Exception.class)
     public void sendWithSubjectFieldEmpty() throws Exception {
-        MailService service = new MailService();
         service.send("gadget.mailsender@gmail.com", "gadget.mailsender@gmail.com", "", "some body.");
     }
 
     @Test(expected = Exception.class)
     public void sendWithBodyFieldEmpty() throws Exception {
-        MailService service = new MailService();
         service.send("gadget.mailsender@gmail.com", "gadget.mailsender@gmail.com", "subject", "");
     }
 
     @Test(expected = Exception.class)
     public void sendWithFromFieldNull() throws Exception {
-        MailService service = new MailService();
         service.send(null, "gadget.mailsender@gmail.com", "subject", "some body.");
     }
 
     @Test(expected = Exception.class)
     public void sendWithToFieldNull() throws Exception {
-        MailService service = new MailService();
         service.send("gadget.mailsender@gmail.com", null, "subject", "some body.");
     }
 
     @Test(expected = Exception.class)
     public void sendWithSubjectFieldNull() throws Exception {
-        MailService service = new MailService();
         service.send("gadget.mailsender@gmail.com", "gadget.mailsender@gmail.com", null, "some body.");
     }
 
     @Test(expected = Exception.class)
     public void sendWithBodyFieldNull() throws Exception {
-        MailService service = new MailService();
         service.send("gadget.mailsender@gmail.com", "gadget.mailsender@gmail.com", "subject", null);
     }
 
