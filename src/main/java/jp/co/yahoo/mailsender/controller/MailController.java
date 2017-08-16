b039ce951ac54aeed5a40fc486d59cd2f97273b0
CSDUser <CSDUser@chip.local>
2017-08-16 13:51:31 +0900

    add MailSendForm, change MailController

--- src/main/java/jp/co/yahoo/mailsender/controller/MailController.java
+++ src/main/java/jp/co/yahoo/mailsender/controller/MailController.java
@@ -1,7 +1,9 @@
 package jp.co.yahoo.mailsender.controller;
 
+import jp.co.yahoo.mailsender.form.MailSendForm;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -9,10 +11,14 @@ import java.util.Map;
 public class MailController {
 
     @GetMapping("/send")
-    public String send(Map<String, Object> request ){
-
+    public String send(Model model){
         return "send";
     }
 
+    @RequestMapping(value = "/send", method = RequestMethod.POST)
+    public String post(@ModelAttribute MailSendForm form, Model model) {
+        model.addAttribute("form", form);
+        return "send";
+    }
 
 }
